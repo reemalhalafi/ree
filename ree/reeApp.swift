527a1d6ff3c994f88593be21781a2c81efa4a8b4
reemalhalafi <alhalafireem@gmail.com>
2023-10-08 10:03:49 +0300

    Initial Commit

---
+++ ree/reeApp.swift
@@ -0,0 +1,17 @@
+//
+//  reeApp.swift
+//  ree
+//
+//  Created by Reem Alhalafi on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct reeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
