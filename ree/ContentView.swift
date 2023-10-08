10d25d8c439e1ffd2b3d2169cffe965a49e9c82b
reemalhalafi <alhalafireem@gmail.com>
2023-10-08 11:39:45 +0300

    trycatch

--- ree/ContentView.swift
+++ ree/ContentView.swift
@@ -10,15 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("reem")
+                .resizable()
+                .clipShape(Circle())
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .overlay(Circle() .stroke(Color.gray,lineWidth: 15))
+                .frame(width: 250.0, height: 250.0, alignment: .center)
+            
+                .clipShape(Circle())
+            
+            Text("Reem Alhalafi")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
+            Text("I am a 22-year-old and i am a fresh graduated from princess nora univercity. I love learning languages and i find learning new things so exciting.")
+            
+                .padding()
         }
-        .padding()
     }
 }
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
+
--- ree/reeApp.swift
+++ ree/reeApp.swift
@@ -12,6 +12,11 @@ struct reeApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+            Image("reem")
+                 .resizable()
+                 .aspectRatio(contentMode: .fill)
+                 .frame(width: 250.0, height: 250.0, alignment: .center)
+                 .border(Color.blue, width: 3.0)
+                 .clipped()        }
     }
 }
